afde535210876e64308cddb8ca3074023bcce6b2
MariaS <pustezora@gmail.com>
2023-05-24 18:22:49 +0200

    Portlio add to GitHub Pages

---
+++ src/data/projects_data.js
@@ -0,0 +1,53 @@
+import bakery from '../media/bakery.png'
+import portfolio from '../media/portfolio.png'
+import garden_store from '../media/garden_store.png'
+import in_progress from '../media/in_progress.jpg'
+import my_portfolio from '../media/my_portfolio.png'
+
+
+
+export const projects_data = [
+    {
+        id: 1,
+        title: 'Bakery (Landing page)',
+        url: 'https://mariaschroder.github.io/Bakery_Landing_page/',
+        git_hub_link: 'https://github.com/MariaSchroder/Bakery_Landing_page',
+        project_img: bakery,
+        technologies: 'HTML, CSS'
+     },
+    {
+        id: 2,
+        title: 'Guitarist Portfolio',
+        url: 'https://mariaschroder.github.io/Guitarist_Portfolio/',
+        git_hub_link: 'https://github.com/MariaSchroder/Guitarist_Portfolio',
+        project_img: portfolio,
+        technologies: 'HTML, SASS/SCSS, Methodology BEM'
+    },
+    {
+        id: 3,
+        title: 'Garden store',
+        url: 'https://online-garden-store.onrender.com/',
+        git_hub_link: 'https://github.com/MariaSchroder/GardenStore_Final_Project',
+        project_img: garden_store,
+        technologies: 'HTML, CSS, JavaScript, React, Redux'
+    },
+    {
+        id: 4,
+        title: 'My portfolio',
+        url: 'https://mariaschroder.github.io/Portfolio/',
+        git_hub_link: 'https://github.com/MariaSchroder/Portfolio',
+        project_img: my_portfolio,
+        technologies: 'HTML, CSS, JavaScript, React'
+    },
+    {
+        id: 5,
+        title: 'Balloon ride travel',
+        url: '',
+        git_hub_link: 'https://github.com/MariaSchroder/Air_Ballon_Tours',
+        project_img: in_progress,
+        technologies: ''
+    },
+
+]
+
+
